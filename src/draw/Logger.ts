4f6f38f71a3a2065c6ce265f8924970627a3f0e6
Joachim Holwech <joachim.holwech@gmail.com>
2019-01-29 20:54:52 +0100

    Started implementing a logger

---
+++ src/draw/Logger.ts
@@ -0,0 +1,14 @@
+interface ILogObject {
+  id: string;
+  type: string;
+  startTime: number;
+
+}
+
+interface ILogPoint {
+  point: { x: string, y: string };
+}
+
+export class Logger {
+  private Log
+}
