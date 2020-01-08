3c4cf6476d8b37941247d5d9625e7ea4bf1bed99
Joachim Holwech <joachim.holwech@gmail.com>
2020-01-08 19:09:03 +0100

    Added an example application

---
+++ example/src/layouts/Toolbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app-bar color="grey darken-3" dark fixed app :collapse="collapseToolbar">
+    <v-layout v-if="!collapseToolbar">
+      <v-toolbar-title class="no-link-style">
+        Draw
+        <sup>
+          <span style="color:yellow">beta</span>
+        </sup>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <slot></slot>
+      </v-toolbar-items>
+    </v-layout>
+    <v-icon
+      v-if="showCollapseButton"
+      @click="collapseToolbar = !collapseToolbar"
+      >{{
+        collapseToolbar ? 'keyboard_arrow_right' : 'keyboard_arrow_left'
+      }}</v-icon
+    >
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
+
+@Component
+export default class Toolbar extends Vue {
+  @Prop({ type: Boolean, default: false })
+  private readonly showCollapseButton?: Boolean;
+
+  private collapseToolbar = false;
+}
+</script>
