eea9a37460ee1f0a452d89275be64b5c1241da4a
Joachim Holwech <joachim.holwech@gmail.com>
2020-02-09 14:34:14 +0100

    Added more options to DispatcherState

--- src/State/DispatcherState.ts
+++ src/State/DispatcherState.ts
@@ -1,11 +1,20 @@
 import { ElementClickACtionType } from '../Interfaces/ActionInterfaces';
 import { singleton } from 'tsyringe';
+import { IStrokeProps } from '../Interfaces/BoardInterfaces';
 
 @singleton()
 export default class DispatcherState {
   public idCount: number = 0;
   public panState: boolean = false;
   public clickTarget = ElementClickACtionType.REMOVE;
+  public strokeProps: IStrokeProps = {
+    stroke: 'green',
+    'stroke-width': 50,
+    'buffer-size': 20,
+    fill: undefined,
+  };
+  public scale: number = 1;
+  public scaledStrokeWidth: number = this.strokeProps["stroke-width"];
 
   constructor() {}
 }
