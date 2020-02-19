90eb6f4996edf6cb4052a075de56acbcb75ef609
Joachim Holwech <joachim.holwech@gmail.com>
2020-02-19 20:18:52 +0100

    Fixed an error where the state would not reset properly

--- src/ServiceBuilder.ts
+++ src/ServiceBuilder.ts
@@ -1,5 +1,5 @@
 import 'reflect-metadata';
-import { container } from 'tsyringe';
+import { container, isValueProvider, DependencyContainer } from 'tsyringe';
 import Service from './Controllers/Service';
 import AppState from './State/AppState';
 import Timer from './Timer/Timer';
@@ -10,14 +10,32 @@ export default class ServiceBuilder {
     if (!(svgElement as SVG).getScreenCTM()) {
       throw new Error('getScreenCTM is not defined');
     }
+    this.clearContainerInstances(container);
     container.register<HTMLElement>(HTMLElement, { useValue: svgElement });
     if (timer != undefined) {
       container.register<Timer>(Timer, { useValue: timer });
+      container.registerInstance
     }
     if (appState != undefined) {
       container.register<AppState>(AppState, { useValue: appState });
     }
     return container.resolve<Service>(Service);
+  }
+  
+  private clearContainerInstances(container: DependencyContainer): void {
+    const registry = (container as any)._registry
+
+    for (const [token, registrations] of registry.entries()) {
+      registry.setAll(
+        token,
+        (registrations as any[])
+          // Clear instances
+          .map(registration => {
+            registration.instance = undefined
+            return registration
+          }),
+      )
+    }
   }
 
   public getContainer() {
